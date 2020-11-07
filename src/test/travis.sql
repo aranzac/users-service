e0411f84053a5160fc70756231d86e9125bb17d4
aranzac <aranza@PC.localdomain>
2020-11-07 17:23:58 +0000

    travis

---
+++ src/test/travis.sql
@@ -0,0 +1,21 @@
+CREATE USER 'dev'@'localhost' IDENTIFIED BY 'dev';
+GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'dev'@'localhost';
+
+
+# Create DB
+CREATE DATABASE IF NOT EXISTS `demo` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+USE `demo`;
+
+
+# Create Table
+CREATE TABLE IF NOT EXISTS `users_db`.`users` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(70) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `state` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 10000073
+DEFAULT CHARACTER SET = utf8;
